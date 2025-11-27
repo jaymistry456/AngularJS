b135a2afb875b511b2cdb39fbca69a8c56c88f69
jaymistry456 <jay.mistry456@yahoo.com>
2025-11-27 19:57:36 -0400

    Complete investment-calculator project and Start learning about components and templates in depth

--- 03-investment-calculator/src/app/app.component.ts
+++ 03-investment-calculator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentData, ResultsData } from './Data.model';
@@ -11,29 +11,5 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  resultsData!: ResultsData[];
-
-  OnCalculateInvestmentResults(data: InvestmentData) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData = annualData;
-  }
+  
 }
--- 04-investment-calculator-using-modules/src/app/investment-results/investment-results.component.ts
+++ 04-investment-calculator-using-modules/src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,9 @@
 import { Component, inject, input, signal } from '@angular/core';
-import { ResultsData } from '../Data.model';
-import { CurrencyPipe } from '@angular/common';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
